da6218117384f381b228a8ae6035caa2a4eaa802
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-09-04 01:31:43 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -2,6 +2,7 @@ let input;
 let total = 0;
 let inputAdd = 0;
 
+// 1 вариант
 // input = prompt();
 // while (input !== null) {
 //   if (isNaN(input)) {
@@ -15,6 +16,7 @@ let inputAdd = 0;
 // }
 // alert(`Общая сумма чисел равна ${total}`);
 
+//  2 вариант
 do {
   input = prompt();
   if (isNaN(input)) {
@@ -24,3 +26,18 @@ do {
   }
 } while (input !== null);
 alert(`Общая сумма чисел равна ${total}`);
+
+// 3 вариант
+// while (true) {
+//   input = prompt();
+//   if (input === null) {
+//     break;
+//   }
+//   input = Number(input);
+//   if (isNaN(input)) {
+//     alert("Было введено не число, попробуйте еще раз");
+//     continue;
+//   }
+//   total += input;
+// }
+// alert(`Общая сумма чисел равна ${total}`);
